15b3173c1bea6883a4f76396fd1fbf8150c1773f
joy777park <joy777park@gmail.com>
2023-11-06 00:18:04 +0800

    加入 OgImage Tag

--- DemoPage/Models/DemoPageViewModel.cs
+++ DemoPage/Models/DemoPageViewModel.cs
@@ -5,5 +5,41 @@ namespace DemoPage.Models
     public class DemoPageViewModel
     {
         public OgImage OgImage { get; set; }
+
+        public string OgImageTag()
+        {
+            if (OgImage.LocalMirrorFile)
+            {
+                return GetExt();
+            }
+            else
+            {
+                return GetLocalExt();
+            }
+        }
+
+        private string GetExt()
+        {
+            if (string.IsNullOrWhiteSpace(OgImage.Ext))
+            {
+                return $"/File/image/{OgImage.Name}{OgImage.Ext}";
+            }
+            else
+            {
+                return $"/File/image/{OgImage.Name}-ogimge{OgImage.Ext}";
+            }
+        }
+
+        private string GetLocalExt()
+        {
+            if (string.IsNullOrWhiteSpace(OgImage.LocalExt))
+            {
+                return $"/File/image/{OgImage.Name}{OgImage.Ext}";
+            }
+            else
+            {
+                return $"/File/image/{OgImage.Name}-ogimge{OgImage.LocalExt}";
+            }
+        }
     }
 }
